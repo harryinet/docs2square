e011022973c24aee56ad8a5a736455fbca0f704a
Harryinet <98124727+harryinet@users.noreply.github.com>
2026-01-18 14:16:59 +0100

    Change pageTitle from 'Quartz 4' to 'Docs2Square'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Docs2Square",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
